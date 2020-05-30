8df2516c0e133ac5d9691619ea0ded2db51b1b4b
John Pham <mr.john.pham@gmail.com>
2020-05-30 00:31:28 -0400

    app.post upload single upfile return json

--- server.js
+++ server.js
@@ -16,6 +16,16 @@ app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
 
+app.post('/', upload.single('upfile'), function (req, res, next) {
+  if (err) {
+    return next(err);
+  }
+  res.json({
+    filename: req.file.filename,
+    size: req.file.size
+  })
+})
+
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
